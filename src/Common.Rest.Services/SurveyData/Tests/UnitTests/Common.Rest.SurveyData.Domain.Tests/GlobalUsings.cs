c230c8b7421f63cc1bfb970f5fa420ef5ccba46b
Ganesh Rajamahanthy <rajamahanthy@gmail.com>
2026-04-24 11:46:39 +0100

    Removed SurveyData

--- src/Common.Rest.Services/SurveyData/Tests/UnitTests/Common.Rest.SurveyData.Domain.Tests/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using Common.Rest.SurveyData.Domain.Entities;
-global using Common.Rest.Shared.Models;
-global using System.Net;
-global using System.Net.Http.Json;
